a4240555ed954cd3500c4fa1194f9352b94bb2c7
Justin Graham <support@pcrobinhood.com>
2019-07-25 23:51:34 -0500

    Testing isomorphic unfetch

--- components/header.js
+++ components/header.js
@@ -9,8 +9,26 @@ const Header = () => (
     <Link href="/">
       <a style={linkStyle}>Home</a>
     </Link>
-    <Link href="/about">
-      <a style={linkStyle}>About</a>
+    <Link href="/whoweare">
+      <a style={linkStyle}>Who We Are</a>
+    </Link>
+    <Link href="/whatwedo">
+      <a style={linkStyle}>What We Do</a>
+    </Link>
+    <Link href="/getinformed">
+      <a style={linkStyle}>Get Informed</a>
+    </Link>
+    <Link href="/getinvolved">
+      <a style={linkStyle}>Get Involved</a>
+    </Link>
+    <Link href="/events">
+      <a style={linkStyle}>Events</a>
+    </Link>
+    <Link href="/contact">
+      <a style={linkStyle}>Contact</a>
+    </Link>
+    <Link href="/donate">
+      <button style={linkStyle}>Donate</button>
     </Link>
   </div>
 );
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,37 @@
-import withLayout from '../components/MyLayout';
+// import withLayout from '../components/MyLayout';
 
-const Page = () => <p>topBar, navigationBar, Carousel, cards</p>;
+// const Page = () => <p>topBar, navigationBar, Carousel, cards, possibly another carousel</p>;
 
-export default withLayout(Page);
+// export default withLayout(Page);
+
+import Layout from '../components/MyLayout.js';
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
+
+const Index = props => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href="/p/[id]" as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+);
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
+
+export default Index;
